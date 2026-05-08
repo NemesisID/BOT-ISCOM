00aab80b1655c8e41877d41c18bcffd9de456251
Ragil Hidayatulloh <ragilhidayah1990@gmail.com>
2026-05-08 17:40:55 +0700

    update fix deployment

--- reo/workflows/sync.py
+++ reo/workflows/sync.py
@@ -10,8 +10,6 @@ async def load_storage():
         storage.guilds.create_table(),
         storage.guilds_log.create_table(),
         storage.users.create_table(),
-        storage.j2c.create_table(),
-        storage.j2c_settings.create_table(),
         storage.antinuke_settings.create_table(),
         storage.antinuke_bypass.create_table(),
         storage.welcomer_settings.create_table(),
@@ -27,13 +25,10 @@ async def load_storage():
         storage.custom_roles_permissions.create_table(),
         storage.media_channels.create_table(),
         storage.auto_responder.create_table(),
-        storage.giveaways.create_table(),
-        storage.giveaway_participants.create_table(),
-        storage.giveaways_permissions.create_table(),
-        storage.ticket_settings.create_table(),
-        storage.tickets.create_table(),
         storage.shop.create_table(),
         storage.music.create_table(),
+        storage.ai_settings.create_table(),
+        storage.counters.create_table(),
     ]
     await asyncio.gather(*tasks)
     logger.database("Database collections loaded")
